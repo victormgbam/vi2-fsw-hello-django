f1dbd66605c436dff1c3f257d626b554fa164cea
victormgbam <victormgbam@hotmail.com>
2023-03-28 14:22:22 +0000

    prepared to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,3 +1,12 @@
+from pathlib import Path
+import os
+
+if os.path.exists('env.py'):
+    import env
+import dj_database_url
+
+
+
 """
 Django settings for django_todo project.
 
@@ -74,11 +83,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
